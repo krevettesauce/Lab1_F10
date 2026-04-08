001dd89fec3afda352da3e5a62fe4e84bb69e9f3
Mikhail Koval <mikhailkoval@icloud.com>
2026-04-08 11:51:57 +0700

    изменение логгирования на Serilog

--- labTestinglab1/Program.cs
+++ labTestinglab1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Globalization;
+using Serilog; // Нужно установить пакет Serilog.Sinks.File и Serilog.Sinks.Console
 
 namespace TriangleCalculator
 {
@@ -9,41 +10,52 @@ namespace TriangleCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите стороны A, B и C (каждая с новой строки):");
-            string inputA = Console.ReadLine();
-            string inputB = Console.ReadLine();
-            string inputC = Console.ReadLine();
+            // Настройка Serilog
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
 
-            var (type, coords) = CalculateTriangle(inputA, inputB, inputC);
+            try
+            {
+                Console.WriteLine("Введите стороны A, B и C:");
+                string inputA = Console.ReadLine();
+                string inputB = Console.ReadLine();
+                string inputC = Console.ReadLine();
 
-            Console.WriteLine($"Тип: {type}");
-            Console.WriteLine($"Координаты: [{string.Join(", ", coords)}]");
+                var (type, coords) = CalculateTriangle(inputA, inputB, inputC);
+
+                Console.WriteLine($"\nРезультат: {type}");
+                Console.WriteLine($"Координаты: {string.Join(", ", coords)}");
+            }
+            finally
+            {
+                Log.CloseAndFlush(); // Важно закрыть логгер в конце
+            }
         }
 
         static (string, List<(int, int)>) CalculateTriangle(string sA, string sB, string sC)
         {
-            string logPath = "log.txt";
-            string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
             string inputs = $"A={sA}, B={sB}, C={sC}";
 
             try
             {
-                // Проверка на валидность чисел
+                // Проверка на числа
                 if (!float.TryParse(sA, NumberStyles.Any, CultureInfo.InvariantCulture, out float a) ||
                     !float.TryParse(sB, NumberStyles.Any, CultureInfo.InvariantCulture, out float b) ||
                     !float.TryParse(sC, NumberStyles.Any, CultureInfo.InvariantCulture, out float c) ||
                     a <= 0 || b <= 0 || c <= 0)
                 {
                     var res = ("", new List<(int, int)> { (-2, -2), (-2, -2), (-2, -2) });
-                    Log(logPath, $"{timestamp} | FAIL | {inputs} | Result: {res.Item1}, Coords: {string.Join(" ", res.Item2)} | Error: Invalid numeric input");
+                    Log.Error("Неуспешный запрос: {Inputs}. Ошибка: Некорректные числа. Результат: {Res}", inputs, res);
                     return res;
                 }
 
-                // Проверка на существование треугольника
+                // Проверка на существование
                 if (a + b <= c || a + c <= b || b + c <= a)
                 {
                     var res = ("не треугольник", new List<(int, int)> { (-1, -1), (-1, -1), (-1, -1) });
-                    Log(logPath, $"{timestamp} | FAIL | {inputs} | Result: {res.Item1}");
+                    Log.Warning("Неуспешный запрос (не треугольник): {Inputs}", inputs);
                     return res;
                 }
 
@@ -52,40 +64,28 @@ namespace TriangleCalculator
                 if (a == b && b == c) type = "равносторонний";
                 else if (a == b || b == c || a == c) type = "равнобедренный";
 
-                // Вычисление координат (вершина A в 0,0; вершина B на оси X)
-                // Используем теорему косинусов для нахождения угла
+                // Координаты (упрощенный расчет)
                 double cosA = (b * b + c * c - a * a) / (2 * b * c);
                 double sinA = Math.Sqrt(1 - cosA * cosA);
-
-                double x1 = 0, y1 = 0;
-                double x2 = c, y2 = 0;
                 double x3 = b * cosA, y3 = b * sinA;
 
-                // Масштабирование в 100x100
                 double maxDim = Math.Max(c, Math.Max(x3, y3));
-                double scale = 100.0 / maxDim;
+                double scale = 100.0 / (maxDim == 0 ? 1 : maxDim);
 
-                var coords = new List<(int, int)>
-                {
-                    ((int)(x1 * scale), (int)(y1 * scale)),
-                    ((int)(x2 * scale), (int)(y2 * scale)),
+                var coords = new List<(int, int)> {
+                    (0, 0),
+                    ((int)(c * scale), 0),
                     ((int)(x3 * scale), (int)(y3 * scale))
                 };
 
-                Log(logPath, $"{timestamp} | SUCCESS | {inputs} | Type: {type} | Coords: {string.Join(" ", coords)}");
+                Log.Information("Успешный запрос: {Inputs}. Тип: {Type}. Координаты: {Coords}", inputs, type, coords);
                 return (type, coords);
             }
             catch (Exception ex)
             {
-                Log(logPath, $"{timestamp} | ERROR | {inputs} | {ex.Message}\n{ex.StackTrace}");
+                Log.Fatal(ex, "Критическая ошибка при обработке запроса: {Inputs}", inputs);
                 return ("", new List<(int, int)> { (-2, -2), (-2, -2), (-2, -2) });
             }
-        }
-
-        static void Log(string path, string message)
-        {
-            Console.WriteLine(message);
-            File.AppendAllText(path, message + Environment.NewLine);
         }
     }
 }
